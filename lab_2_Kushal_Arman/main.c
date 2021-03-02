f573470feea0e207a98cf71087c654a4c5d9fb19
Arman Saduakas <asaduakas@MacBook-Pro-Arman.local>
2021-03-02 00:07:51 -0500

    started with the countdown

--- lab_2_Kushal_Arman/main.c
+++ lab_2_Kushal_Arman/main.c
@@ -5,20 +5,40 @@
 #include <stdio.h>
 
 // Function Prototypes
-void swDelay(char numLoops);
 int check(char array1[], char array2[]);
+void runTimerA2();
+void stopTimerA2();
+void configureRegsiters();
 
+long unsigned int timer=0;
+long unsigned int leapCount = 0;
+int temp = 5;
+
+void configureRegisters(){
+    UCSCTL0 = 0x14B8;
+    UCSCTL1 = 0x0020;
+    UCSCTL2 = 0x101F;
+    UCSCTL3 = 0x0000;
+    UCSCTL4 = 0x0044;
+    UCSCTL5 = 0x0000;
+    UCSCTL6 = 0xC1CD;
+    UCSCTL7 = 0x0403;
+    UCSCTL8 = 0x0707;
+}
 
 // Main
 void main(void)
 
 {
+    configureRegisters();
+    char temp_curr = 0;
     unsigned char currKey=0;        //declaring an unsigned char currKey with value 0 of size of 1 bytes
     int clear_flag = 1;                  //declaring an integer count with value 1 of size 2 bytes
     char number = 0;                //declaring a char number with value 0 of size of 1 bytes
     int i;                          //initializing an integer i
     unsigned char dispThree[3];     ////declaring an unsigned char array dispThree with 3 elements of size of 3 bytes
 
+    _BIS_SR(GIE);                   //enable global interrupt
 
     //setting 1st and 3rd element of dispThree array
     dispThree[0] = ' ';
@@ -30,8 +50,7 @@ void main(void)
     P1DIR |= BIT0;                  // Set P1.0 to output direction
     P4SEL = P4SEL & ~BIT7;          // Select P4.0 for digital IO
     P4DIR |= BIT7;                  // Set P4.0 to output direction
-    __disable_interrupt();// Stop watchdog timer. Always need to stop this!!
-                                 // You can then configure it properly, if desired
+    // You can then configure it properly, if desired
     // Useful code starts here
     initLeds();
     configDisplay();
@@ -43,17 +62,7 @@ void main(void)
     // Draw a box around everything because it looks nice
     Graphics_Rectangle box = {.xMin = 3, .xMax = 125, .yMin = 3, .yMax = 125 };
     Graphics_drawRectangle(&g_sContext, &box);
-
-    // We are now done writing to the display.  However, if we stopped here, we would not
-    // see any changes on the actual LCD.  This is because we need to send our changes
-    // to the LCD, which then refreshes the display.
-    // Since this is a slow operation, it is best to refresh (or "flush") only after
-    // we are done drawing everything we need.
-
     Graphics_flushBuffer(&g_sContext);
-
-    char arr[30] = 0;               ////declaring an char array arr with 30 element, all with value 0 of size 30 bytes
-    char gameInput[30] = 0;         ////declaring an char array gameInput with 30 element, all with value 0 of size 30 bytes
 
     while (1)    // Forever loop
     {
@@ -69,7 +78,7 @@ void main(void)
             currKey = getKey();
             break;
 
-        //List of songs
+            //List of songs
         case '*':
             if(clear_flag){
                 Graphics_clearDisplay(&g_sContext);
@@ -80,18 +89,53 @@ void main(void)
             Graphics_flushBuffer(&g_sContext);
             currKey = getKey();
             if(currKey == '1' || currKey == '2'){
+                temp_curr = currKey;
                 currKey = 'c';
             }
             else{
                 currKey = '*';
             }
             break;
-
-        }
-
         //countdown state
         case 'c':
+            currKey = temp_curr;
+            char i = 3;
+            while(1){
+                runTimerA2();
+                if(timer>32600){
+                    Graphics_clearDisplay(&g_sContext);
+                    Graphics_drawStringCentered(&g_sContext, i , AUTO_STRING_LENGTH, 64, 50, OPAQUE_TEXT);
+                    Graphics_flushBuffer(&g_sContext);
+                    stopTimerA2(1);
+                }
+                i--;
+            }
+        }
+    }
+}
 
+void runTimerA2(void) {
+    // This function configures and starts Timer A2
+    TA2CTL = TASSEL_1 + MC_1 + ID_0;
+    TA2CCR0 = 163;       // 1 second
+    TA2CCTL0 = CCIE;     // TA2CCR0 interrupt enabled
+}
+
+void stopTimerA2(int reset) {   // This function stops Timer A2 andresets the global time variable smj
+    TA2CTL = MC_0;              // stop timer
+    TA2CCTL0 &= ~CCIE;          // TA2CCR0 interrupt disabled
+    if(reset)
+        timer=0;
+}
+
+#pragma vector=TIMER2_A0_VECTOR
+__interrupt void TimerA2_ISR (void) {
+    if(leapCount < 1024){
+        timer++;
+        leapCount++;
+    }
+    else{
+        leapCount = 0;
     }
 }
 
